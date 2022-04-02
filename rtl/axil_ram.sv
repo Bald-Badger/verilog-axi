42e138b243911d8078f4aa08607eb79e1e934a29
Luke <shuaige19981012@gmail.com>
2022-04-02 02:43:18 -0400

    format

--- rtl/axil_ram.sv
+++ rtl/axil_ram.sv
@@ -45,28 +45,28 @@ module axil_ram #
 	parameter PIPELINE_OUTPUT = 0
 )
 (
-	input  wire    clk,
-	input  wire    rst,
+	input  wire						clk,
+	input  wire						rst,
 
-	input  wire [ADDR_WIDTH-1:0]  s_axil_awaddr,
-	input  wire [2:0]        s_axil_awprot,
-	input  wire    s_axil_awvalid,
-	output wire    s_axil_awready,
-	input  wire [DATA_WIDTH-1:0]  s_axil_wdata,
-	input  wire [STRB_WIDTH-1:0]  s_axil_wstrb,
-	input  wire    s_axil_wvalid,
-	output wire    s_axil_wready,
-	output wire [1:0]        s_axil_bresp,
-	output wire    s_axil_bvalid,
-	input  wire    s_axil_bready,
-	input  wire [ADDR_WIDTH-1:0]  s_axil_araddr,
-	input  wire [2:0]        s_axil_arprot,
-	input  wire    s_axil_arvalid,
-	output wire    s_axil_arready,
-	output wire [DATA_WIDTH-1:0]  s_axil_rdata,
-	output wire [1:0]        s_axil_rresp,
-	output wire    s_axil_rvalid,
-	input  wire    s_axil_rready
+	input  wire [ADDR_WIDTH-1:0]	s_axil_awaddr,
+	input  wire [2:0]				s_axil_awprot,
+	input  wire						s_axil_awvalid,
+	output wire						s_axil_awready,
+	input  wire [DATA_WIDTH-1:0]	s_axil_wdata,
+	input  wire [STRB_WIDTH-1:0]	s_axil_wstrb,
+	input  wire						s_axil_wvalid,
+	output wire						s_axil_wready,
+	output wire [1:0]				s_axil_bresp,
+	output wire						s_axil_bvalid,
+	input  wire						s_axil_bready,
+	input  wire [ADDR_WIDTH-1:0]	s_axil_araddr,
+	input  wire [2:0]				s_axil_arprot,
+	input  wire						s_axil_arvalid,
+	output wire						s_axil_arready,
+	output wire [DATA_WIDTH-1:0]	s_axil_rdata,
+	output wire [1:0]				s_axil_rresp,
+	output wire						s_axil_rvalid,
+	input  wire						s_axil_rready
 );
 
 localparam	VALID_ADDR_WIDTH = ADDR_WIDTH - $clog2(STRB_WIDTH);
@@ -118,9 +118,9 @@ initial begin
 		BINARY_BOOT: begin
 			fp = $fopen("instr.bin","r");
 			if (fp == 0) begin
-                $error("failed to open boot file\n");
-                $finish();
-            end
+				$error("failed to open boot file\n");
+				$finish();
+			end
 			s = $fread(mem, fp);
 			$fclose(fp);
 		end
